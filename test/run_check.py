b766313cbd7fc15318eb38fb398f94840636a5e6
Yanjun Qi <qiyanjun2012@gmail.com>
2020-05-05 18:54:47 -0400

    revise check.py

--- test/run_check.py
+++ test/run_check.py
@@ -4,16 +4,29 @@ from sklearn.calibration import CalibratedClassifierCV
 import numpy as np
 from utils import *
 
+import argparse
+
 if __name__ == '__main__':
+
+	parser = argparse.ArgumentParser()
+
+	parser.add_argument('--train', default='../data/EP300.train.fasta',
+		help='training sequences file')
+	parser.add_argument('--test', default='../data/EP300.test.fasta',
+		help='test sequences file')
+
+	args = parser.parse_args()
+	print(args.accumulate(args.integers))
+
 	## Compute kernel matrix
 	fastsk = FastSK(g=10, m=6, t=1, approx=True)
-	fastsk.compute_kernel('../data/EP300.train.fasta', '../data/EP300.test.fasta')
+	fastsk.compute_kernel(args.train, args.test)
 
 	Xtrain = fastsk.get_train_kernel()
 	Xtest = fastsk.get_test_kernel()
 
 	reader = FastaUtility()
-	Xseq, Ytrain = reader.read_data('../data/EP300.train.fasta')
+	Xseq, Ytrain = reader.read_data(args.train)
 
 	## Use linear SVM
 	svm = LinearSVC(C=1)
@@ -21,7 +34,7 @@ if __name__ == '__main__':
 
 	## Evaluate
 	reader = FastaUtility()
-	Xseq, Ytest = reader.read_data('../data/EP300.test.fasta')
+	Xseq, Ytest = reader.read_data(args.test)
 
 	acc = clf.score(Xtest, Ytest)
 	probs = clf.predict_proba(Xtest)[:,1]
